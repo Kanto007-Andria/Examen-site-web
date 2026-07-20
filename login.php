9c422251619db637995c8c41c4ec55ccdd705a7f
Kant007-Andria <kantomaeva42@gmail.com>
2026-07-20 12:33:34 +0300

    login

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Insertion de nom</title>
+</head>
+<body>
+
+    <h2>Ajouter un Nom</h2>
+
+    <form action="traitement_login.php" method="POST">
+        <label for="nom">Nom :</label>
+        <input type="text"  name="nom" placeholder="Entrez votre nom" required>
+        
+        <button type="submit">Entrez</button>
+    </form>
+
+</body>
+</html>
