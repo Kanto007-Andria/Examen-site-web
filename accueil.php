71a0be8d4ec1af27ac5d5ccdc30fd2a551db3712
ETU5014 <idealrdideal@gmail.com>
2026-07-20 11:21:11 +0300

    en tableau

--- accueil.php
+++ accueil.php
@@ -11,25 +11,43 @@
 
     <h1>Liste des produits à vendre</h1>
 
-    <?php foreach($valiny as $produit) { ?>
-        
-        <div>
-            <h3>Produit n°<?php echo $produit['id_produit']; ?></h3>
-            <p>Prix : <?php echo $produit['prix_vente']; ?> €</p>
-            <p>Quantité disponible : <?php echo $produit['quantite_dispo']; ?></p>
-            
-            <form action="traitement_achat.php" method="POST">
-                <input type="hidden" name="id_produit_membre" value="<?php echo $produit['id_produit_membre']; ?>">
-                
-                <p>Quantité :</p>
-                <input type="number" name="quantite" value="1" min="1">
-                
-                <input type="submit" value="Acheter">
-            </form>
-        </div>        
-        <hr>
-        
-    <?php } ?>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Numéro Produit</th>
+                <th>Prix</th>
+                <th>Quantité disponible</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($valiny as $produit) { ?>
+                <tr>
+
+                    <td>Produit n°<?php echo $produit['id_produit']; ?></td>
+                    
+
+                    <td><strong><?php echo $produit['prix_vente']; ?> €</strong></td>
+                    
+
+                    <td><?php echo $produit['quantite_dispo']; ?></td>
+                    
+
+                    <td>
+                        <form action="traitement_achat.php" method="POST" style="margin: 0;">
+                            <input type="hidden" name="id_produit_membre" value="<?php echo $produit['id_produit_membre']; ?>">
+                            
+                            <label>Qté :</label>
+                            <input type="number" name="quantite" value="1" min="1" max="<?php echo $produit['quantite_dispo']; ?>" style="width: 50px;">
+                            
+                            <input type="submit" value="Acheter">
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 
 </body>
 </html>
